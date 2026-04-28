f8aef4bc3e89a1e57bb97b7ee957c123b9e6eb8c
Roman <nechik.biz@gmail.com>
2026-04-28 21:07:17 +0300

    Add OrderService

---
+++ backend/OrderService/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace OrderService.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string UserId { get; set; } = string.Empty; // Кто заказал
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public decimal TotalAmount { get; set; } // Итоговая сумма
+        public string Status { get; set; } = "Pending"; // Статус (Pending, Completed, Cancelled)
+
+        // Список покупок
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ backend/OrderService/Models/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderService.Models
+{
+    public class OrderItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid ProductId { get; set; } // Какой товар
+        public decimal Price { get; set; }  // Цена на момент покупки (фикс!)
+        public int Quantity { get; set; }   // Сколько штук
+
+        // Внешний ключ для связи с заказом
+        public Guid OrderId { get; set; }
+    }
+}
